4207d024ceb16a12698e51dc49fa383281298bb7
faheem4531 <faheem4531@gmail.com>
2024-02-26 15:51:33 +0500

    starting project with mui

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import HomePage from './pages/homePage'
 
 export default function Home() {
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <HomePage />
+    </>
   )
 };
---
+++ src/app/pages/homePage/index.js
@@ -0,0 +1,12 @@
+import NavBar from "@/app/components/navBar/NavBar";
+import { Box } from "@mui/material";
+
+const HomePage = () => {
+  return (
+    <Box>
+      <NavBar />
+    </Box>
+  )
+}
+
+export default HomePage;
