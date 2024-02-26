26991e2e67c7d73e586e53862128d9ba1ef380bc
faheem4531 <faheem4531@gmail.com>
2024-02-26 20:07:32 +0500

    Working section

---
+++ src/app/components/working/Working.js
@@ -0,0 +1,50 @@
+import { Box } from "@mui/material";
+import PrimaryHeading from "../headings/PrimaryHeading";
+import styles from "./Working.module.css"
+import Points from "./Points";
+
+const Working = () => {
+
+  const pointsArray = [
+    {
+      no: "01",
+      title: "Answer questions weekly",
+      discription: "Or as often as you like"
+    },
+    {
+      no: "02",
+      title: "Turn answers into a chapter with a click",
+      discription: "No writing experience needed."
+    },
+    {
+      no: "03",
+      title: "Repeat for all chapters",
+      discription: "As many as you like."
+    },
+    {
+      no: "04",
+      title: "Receive a beautiful hardcover book",
+      discription: "With free shipping."
+    }
+  ]
+
+  return (
+    <Box sx={{ padding: '200px 25px', bgcolor: "#30422E" }}
+      className={styles.workingBox}
+    >
+      <PrimaryHeading heading=" How it works" />
+
+      <Box sx={{ marginTop: "150px", display: "flex", columnGap: "17px" }}>
+
+        {pointsArray.map((item, index) => <Points
+          key={index}
+          no={item.no}
+          title={item.title}
+          discription={item.discription}
+        />)}
+      </Box>
+    </Box>
+  )
+}
+
+export default Working;
--- src/app/pages/homePage/index.js
+++ src/app/pages/homePage/index.js
@@ -1,12 +1,14 @@
 import NavBar from "@/app/components/navBar/NavBar";
 import { Box } from "@mui/material";
 import Introduction from "./sections/introduction/Introduction";
+import Working from "@/app/components/working/Working";
 
 const HomePage = () => {
   return (
     <Box>
       <NavBar />
       <Introduction />
+      <Working />
     </Box>
   )
 }
