123ec6e95f7b43db5b1aa1b6b60bf101deec4c28
faheem4531 <faheem4531@gmail.com>
2024-02-29 19:55:09 +0500

    gif tab

---
+++ src/app/pages/homePage/sections/GifTab.js
@@ -0,0 +1,41 @@
+import { Box, Typography } from "@mui/material";
+import Image from "next/image";
+import styles from "./HomeSections.module.css"
+import Button from "@/app/components/button/Button";
+import Gift from "@/app/_assets/svgs/gift.svg"
+
+const GifTab = () => {
+
+
+  return (
+    <Box sx={{
+      width: "1200px",
+      height: "255px",
+      margin: " 220px auto 0",
+      display: "flex",
+      justifyContent: "space-between",
+      padding: "100px 60px 40px 100px"
+    }}
+      className={styles.gifTabBox}
+    >
+      <Box sx={{ display: "flex", alignItems: "center" }}>
+
+        <Typography sx={{
+          fontSize: "32px", fontWeight: 500, width: "80%"
+        }}>
+          Surprise your loved one with Lifescript and transform their memories into a keepsake book.
+        </Typography>
+        <Button
+          title='Gift Now!'
+          width='180px'
+          height='55px'
+          img1={Gift}
+        />
+
+      </Box>
+    </Box >
+  )
+}
+
+
+export default GifTab;
