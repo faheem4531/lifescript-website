1403a8a2ec62b3d0d7c59c137416225606ffd7f6
faheem4531 <faheem4531@gmail.com>
2024-03-01 18:41:37 +0500

    testimonials........

--- src/app/pages/homePage/sections/Testimonial.js
+++ src/app/pages/homePage/sections/Testimonial.js
@@ -3,9 +3,40 @@ import { Box, Typography, colors } from "@mui/material";
 import styles from "./HomeSections.module.css"
 
 import Image from "next/image";
-import Client from "@/app/_assets/pngs/client-1.png"
+import Client1 from "@/app/_assets/pngs/client-1.png"
+import Client2 from "@/app/_assets/pngs/client-2.png"
+import Client3 from "@/app/_assets/pngs/client-3.png"
+import Client4 from "@/app/_assets/pngs/client-4.png"
 
 const Testimonial = () => {
+  const Client = [
+    {
+      profile: Client1,
+      name: "James T.",
+      designation: "Novelist and Screenwriter",
+      details: "Lifescript transformed the daunting task of documenting my life into a beautifully guided journey. Their intuitive platform made it easy to organize my thoughts, memories, and experiences into a narrative that truly feels like my own. I'm grateful for the chance to share my story with my family in such a meaningful way."
+    },
+    {
+      profile: Client2,
+      name: "Samantha W.",
+      designation: "Entrepreneur",
+      details: "As someone who's always looking forward, taking the time to reflect and write my autobiography with Lifescript was a revelation. The process helped me understand my own path to success, and now I have a legacy to leave behind for future generations. Lifescript's tools and prompts were invaluable in drawing out stories I had forgotten."
+    },
+    {
+      profile: Client3,
+      name: "Ethan K.",
+      designation: "Adventure Traveler",
+      details: "Thanks to Lifescript, I've been able to compile my adventures into an epic narrative that rivals the journeys I've embarked on. The platform's flexibility let me structure my story my way, ensuring that the thrill and lessons from my travels are captured vividly for others to experience."
+    },
+    {
+      profile: Client4,
+      name: "James T.",
+      designation: "Novelist and Screenwriter",
+      details: "Lifescript transformed the daunting task of documenting my life into a beautifully guided journey. Their intuitive platform made it easy to organize my thoughts, memories, and experiences into a narrative that truly feels like my own. I'm grateful for the chance to share my story with my family in such a meaningful way."
+    },
+
+  ]
+
   return (
     <Box>
       <PrimaryHeading showStyle={false} heading="Testimonials" />
@@ -13,40 +44,46 @@ const Testimonial = () => {
         Voices of appreciation: What our customers say
       </Typography>
 
-      <Box>
-        <ReviewCard />
+      <Box sx={{
+        display: "flex", overflowX: "scroll", columnGap: "35px", padding: "0 100px 40px ", scrollbarWidth: "none"
+      }}
+      >
+        {Client.map((item, index) => <ReviewCard
+          key={index}
+          profile={item.profile}
+          name={item.name}
+          designation={item.designation}
+          details={item.details}
+        />)}
       </Box>
-    </Box>
+    </Box >
   )
 }
 
 export default Testimonial
 
-function ReviewCard() {
+function ReviewCard({ profile, name, designation, details }) {
   return (
     <Box sx={{
       boxShadow: " 0px 4px 15px 0px rgba(0, 0, 0, 0.10)",
       borderRadius: "4px",
       padding: "25px 32px 35px 40px",
-      maxWidth: "380px",
+      minWidth: "380px",
       bgcolor: "#F4F4F4"
     }}>
       <Box sx={{ paddingBottom: "15px", display: 'flex', columnGap: "15px", alignItems: "center", fontSize: "20px", borderBottom: "1px solid #BFC4B5" }}>
         <Image
-          src={Client}
+          src={profile}
           alt="image"
           className={styles.clientImg}
         />
         <Box>
-          <Typography sx={{ color: "#E1683B", fontWeight: 800 }}>James T.</Typography>
-          <Typography sx={{ color: "#BFC4B5" }}>Novelist and Screenwriter</Typography>
+          <Typography sx={{ color: "#E1683B", fontWeight: 800 }}>{name}</Typography>
+          <Typography sx={{ color: "#BFC4B5" }}>{designation}</Typography>
         </Box>
       </Box>
       <Typography sx={{ padding: "20px 20px 0 10px", lineHeight: "26px", fontSize: "16px" }}>
-        "Lifescript transformed the daunting task of documenting my life into a beautifully guided journey.
-        Their intuitive platform made it easy to organize my thoughts, memories, and experiences into a
-        narrative that truly feels like my own. I'm grateful for the chance to share my story with my family
-        in such a meaningful way."
+        &quot;{details}&quot;
       </Typography>
     </Box >
   )
