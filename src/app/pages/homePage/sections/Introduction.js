f8921723c311dc9086bf3792be95737b8b0fc626
faheem4531 <faheem4531@gmail.com>
2024-03-01 19:47:21 +0500

    changeover text

--- src/app/pages/homePage/sections/Introduction.js
+++ src/app/pages/homePage/sections/Introduction.js
@@ -1,17 +1,44 @@
+'use client'
+
 import Button from "@/app/components/button/Button";
 import { Box, Typography } from "@mui/material";
 import Pen from "@/app/_assets/svgs/writing-pen.svg"
 import Book from "@/app/_assets/pngs/hero-book.png"
 import Image from "next/image";
 import styles from "./HomeSections.module.css"
+import Typed from "typed.js";
+import { useEffect } from "react";
+
 
 const Introduction = () => {
+
+  const color = { color: "#E1683B" }
+
+  useEffect(() => {
+    const element = document.querySelector(".multiple-text");
+    if (element) {
+      const typed = new Typed(element, {
+        strings: ["Create.", "Gifts."],
+        typeSpeed: 100,
+        backSpeed: 100,
+        delaySpeed: 100,
+        loop: true,
+        showCursor: false,
+      });
+
+      // Cleanup the typed instance when the component unmounts
+      return () => {
+        typed.destroy();
+      };
+    }
+  }, []);
+
   return (
     <Box sx={{ margin: "90px 0 0 75px", position: "relative" }}>
       <Box sx={{ width: '55%' }}>
         <Typography
           sx={{ fontSize: '60px', lineHeight: '70px' }}
-        >The Best Way To Create An Autobiography Book</Typography>
+        >An Autobiography Book <br /> The Best Way To <span className="multiple-text" style={color}></span> </Typography>
         <Typography
           sx={{ fontSize: '16px', lineHeight: '24px', fontWeight: 500, margin: '90px 0 30px', width: '70%' }}
         >An easy-to-use platform enabling people to preserve their memories, rediscover their roots, and
