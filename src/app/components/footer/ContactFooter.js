cf577f546fd91dff1f6c1c6abd30b327f1c98f91
faheem4531 <faheem4531@gmail.com>
2024-03-05 18:41:53 +0500

    features Footer

--- src/app/components/footer/ContactFooter.js
+++ src/app/components/footer/ContactFooter.js
@@ -4,32 +4,31 @@ import styles from "../ComponentsStyles.module.css"
 import Smily from "@/app/_assets/svgs/smily.svg"
 import Button from "../button/Button"
 import Input from "../input/Input"
-import Shape from "@/app/_assets/svgs/input-shape.svg"
 
-const ContactFooter = () => {
+const ContactFooter = ({ title, subTitle, input1, input2, input3, button, shape }) => {
   return (
     <Box
       sx={{ padding: "70px 0 50px", display: "flex", flexDirection: "column", alignItems: "center", color: "#F3ECDA" }}
       className={styles.contactFooterBox}
     >
-      <Typography sx={{ fontSize: "54px", fontWeight: 500, fontFamily: "Besley" }}>Missed the moment? set a reminder!</Typography>
-      <Typography sx={{ fontSize: "16px", fontWeight: 500, marginTop: "30px", fontFamily: "myfamily" }}>Complete the form, and we&apos;ll send you a reminder as your loved one&apos;s special day approaches.</Typography>
+      <Typography sx={{ fontSize: "54px", fontWeight: 500, fontFamily: "Besley" }}>{title}</Typography>
+      <Typography sx={{ fontSize: "16px", fontWeight: 500, marginTop: "30px", fontFamily: "myfamily" }}>{subTitle}</Typography>
 
       <Box sx={{ display: "flex", columnGap: "8px", margin: "55px 0 40px", width: "100%", justifyContent: "center" }}>
         <Input
-          placeHolder="Your email address"
+          placeHolder={input1}
         />
         <Input
-          placeHolder="What&apos;s the occasion?"
+          placeHolder={input2}
         />
         <Input
-          placeHolder="When it&apos;s happening?"
-          img={Shape}
+          placeHolder={input3}
+          img={shape}
         />
       </Box>
 
       <Button
-        title='Remind Me'
+        title={button}
         width='200px'
         height='50px'
         backgroundColor="#30422E"
--- src/app/pages/features/index.js
+++ src/app/pages/features/index.js
@@ -3,12 +3,23 @@
 import { Box } from "@mui/material";
 import FeaturesIntroduction from "./sections/Introduction";
 import FeaturesItems from "./sections/Features";
+import Footer from "@/app/components/footer/Footer";
+import ContactFooter from "@/app/components/footer/ContactFooter";
 
 const Features = () => {
   return (
     <Box>
       <FeaturesIntroduction />
       <FeaturesItems />
+      <ContactFooter 
+      title="Still confused? Ask away!" 
+      subTitle="Contact us Now!!!"
+      input1="Your name"
+      input2="Your email address"
+      input3="What’s on your mind?"
+      button="Get in touch!"
+      />
+      <Footer />
     </Box>
   )
 }
