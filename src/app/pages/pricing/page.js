212921533ed3a2ad9234270c733a4ac48fdac8d0
faheem4531 <faheem4531@gmail.com>
2024-03-06 13:52:03 +0500

    adjusting pricingcards

--- src/app/pages/pricing/page.js
+++ src/app/pages/pricing/page.js
@@ -3,11 +3,19 @@
 import { Box } from "@mui/material";
 import Footer from "@/app/components/footer/Footer";
 import ContactFooter from "@/app/components/footer/ContactFooter";
+import NavBar from "@/app/components/navBar/NavBar";
+import Logo from "@/app/_assets/svgs/logo-footer.svg"
+import styles from "./sections/Pricing.module.css"
+import Pricing from "@/app/components/pricing/Pricing";
 
 const PricingPage = () => {
   return (
-    <Box>
+    <Box sx={{ minHeight: "100vh" }} className={styles.pricingPage}>
+      <NavBar color="#F3ECDA" logo={Logo} />
 
+      <Box>
+        <Pricing />
+      </Box>
       <ContactFooter
         title="Still confused? Ask away!"
         subTitle="Contact us Now!!!"
